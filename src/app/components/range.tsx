1afd054b7bfaff4c87deb9977af4cdf8183de547
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2024-05-02 16:21:35 +0200

    Styling cursor and bullets hover

--- src/app/components/range.tsx
+++ src/app/components/range.tsx
@@ -9,6 +9,7 @@ const Range: React.FC<RangeProps> = ({ min, max }) => {
   const [range, setRange] = useState({ min, max });
   const [dragging, setDragging] = useState<'min' | 'max' | null>(null);
   const rangeRef = useRef<HTMLDivElement>(null);
+  const [hover, setHover] = useState<null | 'min' | 'max'>(null);
 
   const getPercentage = useCallback(
     (value: number) => Math.round(((value - min) / (max - min)) * 100),
@@ -75,69 +76,129 @@ const Range: React.FC<RangeProps> = ({ min, max }) => {
   }, [handleMouseMoveDocument, handleMouseUp]);
   
   return (
-    <div style={{display: "flex", alignContent: "center"}}>
-    <div
-      ref={rangeRef}
+    <div 
       style={{
-        height: '10px',
-        width: '300px',
-        background: 'lightgray',
-        position: 'relative',
-        userSelect: 'none',
+        display: "flex", 
+        alignContent: "center",
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
       }}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
     >
       <div
+        ref={rangeRef}
         style={{
+          height: '10px',
+          width: '300px',
+          background: 'lightgray',
+          borderRadius: '5px',
+          position: 'relative',
+          userSelect: 'none', 
+          cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            background: 'dodgerblue',
+            boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.8)',
+            height: '100%', 
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default',
+            left: `${getPercentage(range.min)}%`,
+            right: `${100 - getPercentage(range.max)}%`,
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            top: hover === 'min' || dragging === 'min' ? '-6px' : '-4px',
+            width:  hover === 'min' || dragging === 'min' ? '22px' : '18px',
+            height:  hover === 'min' || dragging === 'min' ? '22px' : '18px',
+            background: 'white',
+            borderRadius: '50%',
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'pointer',
+            boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.8)',
+            left: `${getPercentage(range.min)}%`,
+            marginLeft: '-10px',
+            transition: 'width 0.2s, height 0.2s, top 0.2s',
+          }}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setDragging('min');
+          }}
+          onMouseEnter={() => setHover('min')}
+          onMouseLeave={() => setHover(null)}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            top: hover === 'max' || dragging === 'max' ? '-6px' : '-4px',
+            width:  hover === 'max' || dragging === 'max' ? '22px' : '18px',
+            height:  hover === 'max' || dragging === 'max' ? '22px' : '18px',
+            background: 'white',
+            borderRadius: '50%',
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'pointer',
+            boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.8)',
+            left: `${getPercentage(range.max)}%`,
+            marginLeft: '-10px',
+            transition: 'width 0.2s, height 0.2s, top 0.2s',
+          }}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setDragging('max');
+          }}
+          onMouseEnter={() => setHover('max')}
+          onMouseLeave={() => setHover(null)}
+        />
+        <div 
+          style={{ 
+            position: 'absolute', 
+            left: "-30px", 
+            top: 0, 
+            fontSize: "12px",
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
+            }}
+          >
+            {min}
+          </div>
+        <div 
+          style={{ 
+            position: 'absolute',
+            right: "-40px", 
+            top: 0, 
+            fontSize: "12px",
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
+            }}
+          >
+            {max}
+          </div>
+      <div 
+        style={{ 
           position: 'absolute',
-          background: 'dodgerblue',
-          height: '100%',
-          left: `${getPercentage(range.min)}%`,
-          right: `${100 - getPercentage(range.max)}%`,
-        }}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          top: '-4px',
-          width: '18px',
-          height: '18px',
-          background: 'white',
-          borderRadius: '50%',
-          cursor: 'pointer',
-          boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.8)',
-          left: `${getPercentage(range.min)}%`,
-          marginLeft: '-10px',
-        }}
-        onMouseDown={(e) => {
-          e.preventDefault();
-          setDragging('min');
-        }}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          top: '-4px',
-          width: '18px',
-          height: '18px',
-          background: 'white',
-          borderRadius: '50%',
-          cursor: 'pointer',
-          boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.8)',
-          left: `${getPercentage(range.max)}%`,
-          marginLeft: '-10px',
-        }}
-        onMouseDown={(e) => {
-          e.preventDefault();
-          setDragging('max');
-        }}
-      />
-      <div style={{ position: 'absolute', left: "-30px", top: 0, fontSize: "12px" }}>{min}</div>
-      <div style={{ position: 'absolute', right: "-40px", top: 0, fontSize: "12px" }}>{max}</div>
-    <div style={{ position: 'absolute', left: `${getPercentage(range.min)-3}%`, top: '30px',fontSize: "12px" }}>{range.min.toFixed(0)}</div>
-      <div style={{ position: 'absolute', left: `${getPercentage(range.max)-3}%`, top: '30px', fontSize: "12px" }}>{range.max.toFixed(0)}</div>
-    </div>
+          left: `${getPercentage(range.min)-3}%`, 
+          top: '30px',
+          fontSize: "12px",
+          cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
+          }}
+        >
+          {range.min.toFixed(0)}
+        </div>
+        <div 
+          style={{ 
+            position: 'absolute',
+            left: `${getPercentage(range.max)-3}%`, 
+            top: '30px', 
+            fontSize: "12px",
+            cursor: dragging ==='min' || dragging === 'max' ? 'col-resize' : 'default'
+          }}
+        >
+          {range.max.toFixed(0)}
+        </div>
+      </div>
     </div>
   );
 };
