c138410c5268b17d80475cb3438eff87ba2ceb96
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2024-05-03 17:14:26 +0200

    npm package prapare

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ mg-range-input is a React-typescript custom range input.
 [![npm](https://img.shields.io/npm/v/mg-range-input)](https://www.npmjs.com/package/mg-range-input)
 
 <p align="center">
-  <img src="" alt="Range input">
+  <img src="https://github.com/miguelgisbert/range-input/blob/master/public/mg-range-input.png" alt="Range input">
 </p>
 
 ## Installation
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-        <Range min={5.99} max={19.99} selectableValues={[0,10,20,30,40,50,60,70,80,90,100]} />
+        <Range min={5.99} max={19.99} />
         <Range min={5.99} max={70.99} selectableValues={[5.99, 10.99, 30.99, 50.99, 70.99]}/>
     <>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div style={{width: "100%", height: "100%", display: "flex", flexDirection: "column", gap: "30px", alignContent:"flex-start", alignItems:"center", justifyContent: "center"}}>
         <h3 style={{ padding: "20px" }}>Exercise 1</h3>
-        <Range min={5.99} max={19.99} selectableValues={[0,10,20,30,40,50,60,70,80,90,100]} />
+        <Range min={5.99} max={19.99} />
         <h3 style={{ padding: "20px" }}>Exercise 2</h3>
-        <Range min={5.99} max={70.99} selectableValues={[5.99, 10.99, 30.99, 50.99, 70.99]}/>
+        <Range min={5.99} max={70.99} selectableValues={[5.99, 10.99, 30.99, 50.99, 70.99]} />
     </div>
   );
 }
