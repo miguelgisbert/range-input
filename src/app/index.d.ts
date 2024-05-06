874f0b54b01f8d5100de0c4d3eeed9642b95b68a
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2024-05-06 17:20:04 +0200

    Fix types definition

--- src/app/index.d.ts
+++ src/app/index.d.ts
@@ -1,1 +1,13 @@
-export { default as Range } from './components/range';
+declare module 'mg-range-input' {
+    import { FC } from 'react';
+  
+    export interface RangeProps {
+      min: number;
+      max: number;
+      selectableValues?: number[];
+    }
+  
+    const Range: FC<RangeProps>;
+  
+    export default Range;
+  }
